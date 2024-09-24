4e776f6e4f7f21b6064fc63800eec95c29693136
Lucas-RM <lucas.marcondes36@gmail.com>
2024-09-24 00:18:20 -0300

    Configurando o projeto com Entity Framework e tabela de administradores

--- Program.cs
+++ Program.cs
@@ -1,9 +1,23 @@
+using MinimalApi.Infraestrutura.Db;
+using MinimalApi.DTOs;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DbContexto>(options =>
+{
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("mysql"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+    );
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapPost("/login", (LoginDTO loginDTO) => {
+app.MapPost("/login", (LoginDTO loginDTO) =>
+{
     if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
         return Results.Ok("Login com sucesso");
     else
@@ -11,9 +25,3 @@ app.MapPost("/login", (LoginDTO loginDTO) => {
 });
 
 app.Run();
-
-public class LoginDTO
-{
-    public string Email { get; set; } = default!;
-    public string Senha { get; set; } = default!;
-}
