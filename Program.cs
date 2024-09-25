03e3af3063619b5c2b0e4b803c0de1b931a4544e
Lucas-RM <lucas.marcondes36@gmail.com>
2024-09-25 20:27:20 -0300

    GET para retornar um veículo

--- Program.cs
+++ Program.cs
@@ -62,6 +62,15 @@ app.MapGet("/veiculos", ([FromQuery] int? pagina, IVeiculoServico veiculoServico
 
     return Results.Ok(veiculos);
 }).WithTags("Veiculos");
+
+app.MapGet("/veiculos/{id}", ([FromRoute] int id, IVeiculoServico veiculoServico) =>
+{
+    var veiculo = veiculoServico.BuscaPorId(id);
+
+    if(veiculo == null) return  Results.NotFound();
+ 
+    return Results.Ok(veiculo);
+}).WithTags("Veiculos");
 #endregion
 
 #region App
